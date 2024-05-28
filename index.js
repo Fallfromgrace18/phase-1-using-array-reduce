74857248aab8c706e7fa2e8e3af31c049c94a6a0
Fallfromgrace18 <149462981+Fallfromgrace18@users.noreply.github.com>
2024-05-28 18:44:13 -0400

    completed

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const initialValue=batteryBatches[0]
+const currentValue=batteryBatches[0]
+const totalBatteries=
+batteryBatches.reduce((accumulator, currentValue)=>
+    accumulator+currentValue)
+console.log(totalBatteries)
